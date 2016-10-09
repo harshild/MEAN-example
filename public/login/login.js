7534aee69ef6b5a628069aa9cc748583bacb8e38
Harshild <harshil.dhariwal@gmail.com>
2016-10-09 13:44:15 +0530

    REST API Demonstration

--- public/login/login.js
+++ public/login/login.js
@@ -1,13 +1,41 @@
 angular.module('testApp.loginctrl',[])
-        .controller('loginctrl', function ($scope) {
-            $scope.firstname = "";
-            $scope.lastname = "";
+    .controller('loginctrl', function ($scope,$http) {
+        $scope.firstname = "";
+        $scope.lastname = "";
 
-            $scope.dataEntered = function () {
-                $scope.fullname = $scope.firstname + " " + $scope.lastname;
-            }
+        var isUserValid = false;
+        $scope.dataEntered = function () {
+            $scope.fullname = ($scope.firstname +
+                " " + $scope.lastname ).trim();
 
-            $scope.isFullNameAvailable = function () {
-                return $scope.fullname && $scope.fullname != "";
-            }
-        });
+            validateUser($scope.fullname);
+        }
+
+        $scope.isFullNameAvailable = function () {
+            return $scope.fullname
+                && $scope.fullname != ""
+                && isUserValid;
+        }
+
+        var validateUser = function (fullName) {
+            $http({
+                method: 'POST',
+                url: '/validate',
+                data: {name :fullName}
+            }).
+            success(function (res) {
+                if(res == "yes")
+                    isUserValid = true
+                if(res == "no")
+                    isUserValid = false
+
+            }).
+            error(function (res) {
+                isUserValid = false
+
+            });
+        }
+
+
+
+    });
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require('express');
+var bodyParser  =  require('body-parser');
 var app = express();
 
 var port = 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
 
 app.listen(port);
 
-app.get('/validate', function(req, res, next) {
-    console.log(req)
-    return "";
+app.post('/validate', function(req, res) {
+    var name = req.body.name;
+    console.log(name);
+    console.log(name.split(" ").length);
+    if(name.split(" ").length == 2)
+        res.end("yes")
+    else
+        res.end("no")
+
+
 });
 
 
